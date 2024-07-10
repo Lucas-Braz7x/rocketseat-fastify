563e03a9815d189d7caa3615283b85dfbb73efcf
ldutra <ldutra@rpc.com.br>
2024-07-09 22:03:58 -0300

    adicionando variáveis de ambiente

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import fastify from 'fastify'
+// import crypto from 'node:crypto'
 import { knex } from './config/db'
+import { env } from './env'
 
 const app = fastify() // base da aplicação
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  // INSERT
+  /* const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'First Transaction',
+      amount: 2500,
+    })
+    .returning('*') */
 
-  return tables
+  // SELECT
+  const transaction = await knex('transactions')
+    .where('amount', 2500)
+    .select('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log('Https server is running... '))
